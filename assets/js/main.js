79052ef154de8e9646d4b15ec9168b295e1b8e17
vovan <vovanshil95@mail.ru>
2022-11-23 21:00:54 +0300

    add everithing on dev1 because dev wasnt conected to main

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+// MENU SHOW Y HIDDEN
+
+const navMenu = document.getElementById('nav-menu'), 
+      navToggle = document.getElementById('nav-toggle')
+      navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const skillsContent = document.getElementsByClassName('skills__content'), 
+skillsHeader = document.querySelectorAll('.skills__header')
+
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for (let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+
+
+const modalViews = document.querySelectorAll('.services__modal'),
+modalBtns = document.querySelectorAll('.services__button'),
+modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+let swiper = new Swiper('.swiper', {
+
+    cssMode: true,
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    mouseWheel: true,
+    keyboard: true,
+
+  });
